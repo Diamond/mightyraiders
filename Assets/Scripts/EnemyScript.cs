38c8d7b6ca854098d9599cee04242377b8933fc8
Brandon Richey <diamondgfx@gmail.com>
2014-10-26 22:22:44 -0400

    Initial prototype

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+	private GameControllerScript _gcScript;
+	public  float                maxHp ;
+	public  float				 currentHp;
+	public  float 
+
+	void Start() {
+		_gcScript = GameObject.Find ("GameController").GetComponent<GameControllerScript>();
+	}
+
+	void OnMouseDown() {
+		_gcScript.AttackEnemy();
+	}
+
+	public void SpawnEnemy() {
+
+	}
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+	public Text enemyHpDisplay;
+	public Text playerDpsDisplay;
+
+	public float enemyHp   = 100.0f;
+	public float playerDps = 1.0f;
+
+	private float _timeSinceLastUpdate = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		enemyHp = 100.0f;
+		enemyHpDisplay.text = "Enemy HP\n" + Mathf.RoundToInt(enemyHp) + "/100";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_timeSinceLastUpdate += Time.deltaTime;
+		if (_timeSinceLastUpdate >= 1.0f) {
+			_timeSinceLastUpdate = 0.0f;
+			AttackEnemy();
+		}
+	}
+
+	void UpdateDisplay() {
+		enemyHpDisplay.text = "Enemy HP\n" + Mathf.RoundToInt(enemyHp) + "/100";
+	}
+
+	public void AttackEnemy() {
+		enemyHp -= playerDps;
+		UpdateDisplay();
+	}
+}
